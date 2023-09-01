b4632b3e93e5b761ee821a51c5a970b33b78211d
Vampire010 <44545166+Vampire010@users.noreply.github.com>
2023-09-01 08:38:13 +0530

    01-Sep-2023

--- src/Day5/Customer.java
+++ src/Day5/Customer.java
@@ -2,19 +2,20 @@ package Day5;
 
 public class Customer
 {
+	//States / Attributes
 	String custID ;
 	String CustName;
 	String custAddress;
 	long custConatct;
 	
+	
+	//Behaviour // Implentation // Functionality // Method
 	public void displayCustomerDetails()
 	{
 		System.out.println("Customer ID: " + custID);
 		System.out.println("Customer Name: " + CustName);
 		System.out.println("Customer Address: " + custAddress);
 		System.out.println("Customer Contact: " + custConatct);
-
-		
 	}
 
 	
